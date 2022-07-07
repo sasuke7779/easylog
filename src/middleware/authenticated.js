b04ce15948fb51e6f1b69a8d0c38c65c907248de
sasuke7779 <sasuke7779@gmail.com>
2022-07-08 08:37:01 +0900

    not login redirect index

--- src/middleware/authenticated.js
+++ src/middleware/authenticated.js
@@ -2,18 +2,16 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 export default async ({ store, route, redirect }) => {
   if (!store.getters.getUserUid) {
     const auth = getAuth();
-    console.log('1');
-    let hoge = await onAuthStateChanged(auth, (user) => {
-      return new Promise((resolve) => {
-        user = user ? user : {};
-        store.dispatch('onAuth', { user: user });
-        console.log('2');
-      });
-    });
-  }
 
-  if (!store.getters.getUserUid && route.name !== 'index') {
-    // store.dispatch('onAuth');
-    // redirect('/');
+    onAuthStateChanged(auth, (user) => {
+      user = user ? user : {};
+      const loginStatus = user.uid ? true : false;
+
+      store.dispatch('onAuth', { user: user });
+
+      if (!loginStatus && route.name !== 'index') {
+        redirect('/');
+      }
+    });
   }
 };
